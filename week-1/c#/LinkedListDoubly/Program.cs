7c7a2d18a93c1319949de134ef633a0131152980
NotHydra <rizky.irswanda115@gmail.com>
2024-09-05 22:16:17 +0800

    feat(week-1): linked list doubly add & pop

--- week-1/c#/LinkedListDoubly/Program.cs
+++ week-1/c#/LinkedListDoubly/Program.cs
@@ -1,2 +1,168 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LinkedListDoubly
+{
+    class Node<Type>(Type value)
+    {
+        private readonly Type _value = value;
+        private Node<Type>? _next = null;
+        private Node<Type>? _previous = null;
+
+        public Type Value
+        {
+            get { return this._value; }
+        }
+
+        public Node<Type>? Next
+        {
+            get { return this._next; }
+            set { this._next = value; }
+        }
+
+        public Node<Type>? Previous
+        {
+            get { return this._previous; }
+            set { this._previous = value; }
+        }
+    };
+
+    class LinkedList<Type>
+    {
+        private Node<Type>? _first = null;
+        private Node<Type>? _last = null;
+
+        public Node<Type>? First
+        {
+            get { return this._first; }
+            set { this._first = value; }
+        }
+
+        public Node<Type>? Last
+        {
+            get { return this._last; }
+            set { this._last = value; }
+        }
+
+        public int Length()
+        {
+            int count = 0;
+            Node<Type>? currentNode = this.First;
+            while (currentNode != null)
+            {
+                currentNode = currentNode.Next;
+                count++;
+            };
+
+            return count;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(this);
+        }
+
+        public void Debug()
+        {
+            Console.WriteLine("Debug: Start");
+
+            Node<Type>? currentNode = this.First;
+            while (currentNode != null)
+            {
+                string? previousText = (currentNode.Previous != null && currentNode.Previous.Value != null) ? currentNode.Previous.Value.ToString() : "null";
+                string? nextText = (currentNode.Next != null && currentNode.Next.Value != null) ? currentNode.Next.Value.ToString() : "null";
+
+                Console.WriteLine($"Debug: {previousText} <- {currentNode.Value} -> {nextText}");
+
+                currentNode = currentNode.Next;
+            };
+
+            Console.WriteLine("Debug: End");
+        }
+
+        public void Add(Type value)
+        {
+            Node<Type> newNode = new Node<Type>(value);
+            if (this.First == null || this.Last == null)
+            {
+                this.First = newNode;
+                this.Last = newNode;
+
+                return;
+            };
+
+            this.Last.Next = newNode;
+            newNode.Previous = this.Last;
+            this.Last = newNode;
+        }
+
+        public Type Pop()
+        {
+            if (this.First == null || this.Last == null)
+            {
+                throw new InvalidCastException("Linked List is empty");
+            };
+
+            Type popValue = this.Last.Value;
+            if (this.First == this.Last)
+            {
+                this.First = null;
+                this.Last = null;
+
+                return popValue;
+            };
+
+            this.Last = this.Last.Previous;
+            if (this.Last != null)
+            {
+                this.Last.Next = null;
+            };
+
+            return popValue;
+        }
+
+        public override string ToString()
+        {
+            string text = "null -> ";
+            Node<Type>? currentNode = this.First;
+            while (currentNode != null)
+            {
+                text += $"{currentNode.Value} -> ";
+                currentNode = currentNode.Next;
+            };
+
+            text += "null";
+
+            return text;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> linkedList1 = new LinkedList<int>();
+            linkedList1.Add(7);
+            linkedList1.Add(8);
+            linkedList1.Add(9);
+
+            Console.Write("Show: ");
+            linkedList1.Show();
+            Console.WriteLine();
+
+            linkedList1.Debug();
+            Console.WriteLine();
+
+            Console.WriteLine($"Length: {linkedList1.Length()}");
+            Console.WriteLine();
+
+            Console.WriteLine($"First: {linkedList1.First?.Value}");
+            Console.WriteLine($"Last: {linkedList1.Last?.Value}");
+            Console.WriteLine();
+
+            Console.Write($"Pop: {linkedList1.Pop()} => ");
+            linkedList1.Show();
+            Console.WriteLine();
+
+            linkedList1.Debug();
+            Console.WriteLine();
+        }
+    }
+}
