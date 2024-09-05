9fdf3901291d6d5fd14df8dbfd10cfb80462d2ec
NotHydra <rizky.irswanda115@gmail.com>
2024-09-05 11:24:03 +0800

    feat(week-1): add reference for logic checking

---
+++ week-1/c#/Reference/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Reference
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList1 = new();
+            linkedList1.AddLast(1);
+            Console.WriteLine(linkedList1.Count);
+            linkedList1.RemoveLast();
+            Console.WriteLine(linkedList1.Count);
+        }
+    }
+}
