4217b13a3946910414a4cb2deb9143afb632d942
NotHydra <rizky.irswanda115@gmail.com>
2024-09-06 18:22:01 +0800

    refactor(week-2): block scoped namespace

--- week-2/StackAndQueue/Nodes/DoubleLinkedNode.cs
+++ week-2/StackAndQueue/Nodes/DoubleLinkedNode.cs
@@ -1,12 +1,13 @@
-namespace StackAndQueue.Classes;
-
-public class DoubleLinkedNode<Type>(Type value): Node<Type>(value)
+namespace StackAndQueue.Nodes
 {
-    private Node<Type>? _prev = null;
-    
-    public Node<Type>? Prev
+    public class DoubleLinkedNode<Type>(Type value) : Node<Type>(value)
     {
-        get => this._prev;
-        set => this._prev = value;
+        private Node<Type>? _prev = null;
+
+        public Node<Type>? Prev
+        {
+            get { return this._prev; }
+            set { this._prev = value; }
+        }
     }
 }
--- week-2/StackAndQueue/Nodes/Node.cs
+++ week-2/StackAndQueue/Nodes/Node.cs
@@ -1,19 +1,19 @@
-namespace StackAndQueue.Classes;
-
-public class Node<Type>(Type value)
+namespace StackAndQueue.Nodes
 {
+    public class Node<Type>(Type value)
+    {
         private Type _value = value;
         private Node<Type>? _next = null;
 
         public Type Value
         {
-            get => this._value;
-            set => this._value = value;
+            get { return this._value; }
         }
 
         public Node<Type>? Next
         {
-            get => this._next;
-            set => this._next = value;
+            get { return this._next; }
+            set { this._next = value; }
         }
+    }
 }
