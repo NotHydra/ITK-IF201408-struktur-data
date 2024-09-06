5e147dda29da1ce3cebd219ebb6750fde433f267
NotHydra <rizky.irswanda115@gmail.com>
2024-09-06 15:11:21 +0800

    feat(week-1): linked list doubly insert

--- week-1/c#/LinkedListDoubly/Program.cs
+++ week-1/c#/LinkedListDoubly/Program.cs
@@ -66,8 +66,8 @@
             Node<Type>? currentNode = this.First;
             while (currentNode != null)
             {
-                string? previousText = (currentNode.Previous != null && currentNode.Previous.Value != null) ? currentNode.Previous.Value.ToString() : "null";
-                string? nextText = (currentNode.Next != null && currentNode.Next.Value != null) ? currentNode.Next.Value.ToString() : "null";
+                string previousText = (currentNode.Previous != null) ? currentNode.Previous.Value!.ToString()! : "null";
+                string nextText = (currentNode.Next != null) ? currentNode.Next.Value!.ToString()! : "null";
 
                 Console.WriteLine($"Debug: {previousText} <- {currentNode.Value} -> {nextText}");
 
@@ -79,7 +79,7 @@
 
         public void Add(Type value)
         {
-            Node<Type> newNode = new Node<Type>(value);
+            Node<Type> newNode = new(value);
             if (this.First == null || this.Last == null)
             {
                 this.First = newNode;
@@ -97,7 +97,7 @@
         {
             if (this.First == null || this.Last == null)
             {
-                throw new InvalidCastException("Linked List is empty");
+                throw new InvalidOperationException("Linked List is empty");
             };
 
             Type popValue = this.Last.Value;
@@ -136,9 +136,9 @@
             {
                 currentNode = this.First;
                 currentIndex = 0;
-                while ((currentNode.Next != null) && (currentIndex < index))
+                while (currentIndex < index)
                 {
-                    currentNode = currentNode.Next;
+                    currentNode = currentNode.Next!;
                     currentIndex++;
                 };
 
@@ -147,13 +147,73 @@
 
             currentNode = this.Last;
             currentIndex = this.Length() - 1;
-            while ((currentNode.Previous != null) && (currentIndex > index))
+            while (currentIndex > index)
             {
-                currentNode = currentNode.Previous;
+                currentNode = currentNode.Previous!;
                 currentIndex--;
             };
 
             return currentNode.Value;
+        }
+
+        public void Insert(Type value, int index)
+        {
+            if ((index < 0) || (this.Length() < index))
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index out of bound");
+            };
+
+            Node<Type> newNode = new(value);
+            if (index == 0)
+            {
+                if (this.First == null || this.Last == null)
+                {
+                    this.First = newNode;
+                    this.Last = newNode;
+
+                    return;
+                };
+
+                newNode.Next = this.First;
+                this.First.Previous = newNode;
+                this.First = newNode;
+
+                return;
+            };
+
+            if (index == this.Length())
+            {
+                this.Last!.Next = newNode;
+                newNode.Previous = this.Last;
+                this.Last = newNode;
+
+                return;
+            };
+
+            int currentIndex;
+            Node<Type> currentNode;
+            if (index < (this.Length() / 2))
+            {
+                currentIndex = 0;
+                currentNode = this.First!;
+                while (currentIndex < index)
+                {
+                    currentNode = currentNode.Next!;
+                    currentIndex++;
+                };
+            }
+            else
+            {
+                currentIndex = this.Length() - 1;
+                currentNode = this.Last!;
+                while (currentIndex > index)
+                {
+                    currentNode = currentNode.Previous!;
+                    currentIndex--;
+                };
+            };
+
+            newNode.Next = currentNode;
         }
 
         public override string ToString()
@@ -176,7 +236,7 @@
     {
         public static void Main(string[] args)
         {
-            LinkedList<int> linkedList1 = new LinkedList<int>();
+            LinkedList<int> linkedList1 = new();
             linkedList1.Add(7);
             linkedList1.Add(8);
             linkedList1.Add(9);
