c8504b95421ba9d4f3c21f98c00af9e00b859be4
NotHydra <rizky.irswanda115@gmail.com>
2024-11-08 21:53:25 +0800

    feat(week-9 > visualization): line

--- week-9/visualization/src/App.tsx
+++ week-9/visualization/src/App.tsx
@@ -1,8 +1,105 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Node, BinarySearchTree } from "./structures/binary-search-tree";
 
 export default function App() {
+	const canvasRef = useRef<SVGSVGElement | null>(null);
 	const [bst, setBST] = useState(new BinarySearchTree<string>());
+
+	const getElementPosition = (
+		element: HTMLElement | null
+	): {
+		x: number;
+		y: number;
+	} => {
+		if (!element) {
+			return { x: 0, y: 0 };
+		}
+
+		const rect = element.getBoundingClientRect();
+		return {
+			x: rect.left + rect.width / 2,
+			y: rect.top + rect.height / 2,
+		};
+	};
+
+	const drawLine = () => {
+		if (canvasRef.current === null) {
+			return;
+		}
+
+		canvasRef.current.innerHTML = "";
+
+		Object.values(bst.getNodesByDepth()).forEach(
+			(nodes: (Node<string> | string)[]) => {
+				nodes.forEach((node: Node<string> | string) => {
+					if (node === "null" || node === "empty") {
+						return;
+					}
+
+					const nodeElement = document.getElementById(
+						`node-${(node as Node<string>).key}`
+					);
+
+					if (nodeElement === null) {
+						return;
+					}
+
+					const nodePosition = getElementPosition(nodeElement);
+
+					const leftNode = (node as Node<string>).left;
+					const rightNode = (node as Node<string>).right;
+
+					if (leftNode) {
+						const leftNodeElement = document.getElementById(
+							`node-${leftNode.key}`
+						);
+
+						if (leftNodeElement) {
+							const leftChildPosition =
+								getElementPosition(leftNodeElement);
+
+							const line = document.createElementNS(
+								"http://www.w3.org/2000/svg",
+								"line"
+							);
+							line.setAttribute("x1", `${nodePosition.x}`);
+							line.setAttribute("y1", `${nodePosition.y}`);
+							line.setAttribute("x2", `${leftChildPosition.x}`);
+							line.setAttribute("y2", `${leftChildPosition.y}`);
+							line.setAttribute("stroke", "black");
+							line.setAttribute("stroke-width", "2");
+
+							canvasRef.current!.appendChild(line);
+						}
+					}
+
+					if (rightNode) {
+						const rightNodeElement = document.getElementById(
+							`node-${rightNode.key}`
+						);
+
+						if (rightNodeElement) {
+							const rightChildPosition =
+								getElementPosition(rightNodeElement);
+
+							const line = document.createElementNS(
+								"http://www.w3.org/2000/svg",
+								"line"
+							);
+							line.setAttribute("x1", `${nodePosition.x}`);
+							line.setAttribute("y1", `${nodePosition.y}`);
+							line.setAttribute("x2", `${rightChildPosition.x}`);
+							line.setAttribute("y2", `${rightChildPosition.y}`);
+							line.setAttribute("stroke", "black");
+							line.setAttribute("stroke-width", "2");
+
+							canvasRef.current!.appendChild(line);
+						}
+					}
+				});
+			}
+		);
+	};
 
 	useEffect(() => {
 		const newBST = new BinarySearchTree<string>();
@@ -21,6 +118,15 @@ export default function App() {
 		setBST(newBST);
 	}, []);
 
+	useEffect(() => {
+		drawLine();
+
+		window.addEventListener("resize", drawLine);
+		return () => {
+			window.removeEventListener("resize", drawLine);
+		};
+	}, [bst]);
+
 	return (
 		<>
 			{Object.values(bst.getNodesByDepth()).map(
@@ -36,10 +142,13 @@ export default function App() {
 							): JSX.Element => {
 								let nodeType: string;
 								let nodeValue: string;
-								if (node === "null") {
-									nodeType = "null";
-									nodeValue = "null";
-								} else if (node === "empty") {
+
+								// if (node === "null") {
+								// 	nodeType = "null";
+								// 	nodeValue = "null";
+								// }
+
+								if (node === "null" || node === "empty") {
 									nodeType = "empty";
 									nodeValue = "";
 								} else {
@@ -50,6 +159,7 @@ export default function App() {
 								return (
 									<span
 										key={nodeIndex}
+										id={`node-${nodeValue}`}
 										className={`${
 											nodeType === "empty"
 												? "empty"
@@ -64,6 +174,8 @@ export default function App() {
 					</div>
 				)
 			)}
+
+			<svg ref={canvasRef}></svg>
 		</>
 	);
 }
