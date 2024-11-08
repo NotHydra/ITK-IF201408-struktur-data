6c7cac1bcfe26113b71699357e02d55b6b7d9d3a
NotHydra <rizky.irswanda115@gmail.com>
2024-11-08 13:00:47 +0800

    refactor(week-9 > visualization): binary search tree

--- week-9/visualization/src/App.tsx
+++ week-9/visualization/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BinarySearchTree } from "./structure/binary-search-tree";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const bst1: BinarySearchTree<string> = new BinarySearchTree<string>();
+	bst1.add("C");
+	bst1.add("H");
+	bst1.add("A");
+	bst1.add("N");
+	bst1.add("D");
+	bst1.add("R");
+	bst1.add("A");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	console.log(bst1.isExist("A"));
+	console.log(bst1.isExist("B"));
+	console.log(bst1.preOder());
+	console.log(bst1.inOrder());
+	console.log(bst1.postOrder());
+	console.log(bst1.toString());
+
+	bst1.show();
+	bst1.remove("D");
+	bst1.show();
+
+	return <>Test</>;
 }
-
-export default App
