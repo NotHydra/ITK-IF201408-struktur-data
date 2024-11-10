aa24efcd224d871e3021b17f15cab11c553c010d
NotHydra <rizky.irswanda115@gmail.com>
2024-11-10 22:33:51 +0800

    feat(week-11): remove

--- week-11/RedBlackTree/Structure/RedBlackTree.cs
+++ week-11/RedBlackTree/Structure/RedBlackTree.cs
@@ -3,12 +3,13 @@ namespace RedBlackTree.Structure
     public enum KeyDirection
     {
         Left,
+        Middle,
         Right,
     }
 
     public class Node<TypeKey>(TypeKey key, bool red, Node<TypeKey>? parent)
     {
-        private readonly TypeKey? _key = key;
+        private TypeKey? _key = key;
         private bool _red = red;
         private Node<TypeKey>? _parent = parent;
         private Node<TypeKey>? _right = null;
@@ -17,6 +18,7 @@ namespace RedBlackTree.Structure
         public TypeKey? Key
         {
             get { return this._key; }
+            set { this._key = value; }
         }
 
         public bool Red
@@ -64,15 +66,35 @@ namespace RedBlackTree.Structure
             Console.WriteLine(this);
         }
 
+        private Node<TypeKey> GetMin(Node<TypeKey> node)
+        {
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+
+            return node;
+        }
+
+        private Node<TypeKey> GetMax(Node<TypeKey> node)
+        {
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+
+            return node;
+        }
+
         private KeyDirection CompareKeyDirection(TypeKey key1, TypeKey key2)
         {
             if (typeof(TypeKey) == typeof(int))
             {
-                return (int)(object)key1! < (int)(object)key2! ? KeyDirection.Left : KeyDirection.Right;
+                return (int)(object)key1! < (int)(object)key2! ? KeyDirection.Left : ((int)(object)key1! == (int)(object)key2! ? KeyDirection.Middle : KeyDirection.Right);
             }
             else
             {
-                return (char)(object)key1! < (char)(object)key2! ? KeyDirection.Left : KeyDirection.Right;
+                return (char)(object)key1! < (char)(object)key2! ? KeyDirection.Left : ((char)(object)key1! == (char)(object)key2! ? KeyDirection.Middle : KeyDirection.Right);
             }
         }
 
@@ -335,6 +357,271 @@ namespace RedBlackTree.Structure
                 node.Red = false;
                 node.Left!.Red = true;
             }
+        }
+
+        public bool Remove(TypeKey key)
+        {
+            if (!((typeof(TypeKey) != typeof(int)) || (typeof(TypeKey) != typeof(char))))
+            {
+                throw new ArgumentException("Type must be int or char");
+            }
+
+            if (this.Root == null)
+            {
+                throw new InvalidOperationException("Tree is empty");
+            };
+
+            if (!this.IsExist(key))
+            {
+                return false;
+            }
+
+            Node<TypeKey> node = Remove(this.Root, key);
+            node = RemoveSimpleCases(node)!;
+
+            if (node == null)
+            {
+                return true;
+            }
+
+            RemoveLeaf(node.Parent!, CompareKeyDirection(node.Key!, node.Parent!.Key!));
+
+            do
+            {
+                node = RemoveColor(node)!;
+            }
+            while (node != null && node.Parent != null);
+
+            return true;
+        }
+
+        private Node<TypeKey> Remove(Node<TypeKey> node, TypeKey key)
+        {
+            while (true)
+            {
+                if (this.CompareKeyDirection(key, node.Key!) == KeyDirection.Left)
+                {
+                    node = node.Left!;
+                }
+                else if (this.CompareKeyDirection(key, node.Key!) == KeyDirection.Right)
+                {
+                    node = node.Right!;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return node;
+        }
+
+        private int GetRemoveCase(Node<TypeKey> node)
+        {
+            KeyDirection direction = this.CompareKeyDirection(node.Key!, node.Parent!.Key!);
+
+            Node<TypeKey> sibling = direction == KeyDirection.Left ? node.Parent!.Right! : node.Parent!.Left!;
+            Node<TypeKey> closeNephew = direction == KeyDirection.Left ? sibling.Left! : sibling.Right!;
+            Node<TypeKey> farNephew = direction == KeyDirection.Left ? sibling.Right! : sibling.Left!;
+
+            if (sibling.Red == true)
+            {
+                return 3;
+            }
+            else if (farNephew != null && farNephew.Red == true)
+            {
+                return 6;
+            }
+            else if (closeNephew != null && closeNephew.Red == true)
+            {
+                return 5;
+            }
+            else if (node.Parent!.Red == true)
+            {
+                return 4;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private Node<TypeKey>? RemoveSimpleCases(Node<TypeKey> node)
+        {
+            if (node.Parent == null && node.Left == null && node.Right == null)
+            {
+                this.Root = null;
+
+                return null;
+            }
+
+            if (node.Left != null && node.Right != null)
+            {
+                Node<TypeKey> successor = GetMin(node.Right!);
+                node.Key = successor.Key;
+                node = successor;
+            }
+
+            if (node.Red == true)
+            {
+                RemoveLeaf(node.Parent!, CompareKeyDirection(node.Key!, node.Parent!.Key!));
+
+                return null;
+            }
+            else
+            {
+                return RemoveBlackNode(node);
+            }
+        }
+
+        private void RemoveLeaf(Node<TypeKey> node, KeyDirection childDirection)
+        {
+            if (childDirection == KeyDirection.Left)
+            {
+                node.Left = null;
+            }
+            else
+            {
+                node.Right = null;
+            }
+        }
+
+        private Node<TypeKey>? RemoveBlackNode(Node<TypeKey> node)
+        {
+            Node<TypeKey>? child = node.Left ?? node.Right;
+
+            if (child == null)
+            {
+                return node;
+            }
+
+            child.Red = false;
+            child.Parent = node.Parent;
+
+            KeyDirection childDirection = node.Parent == null ? KeyDirection.Middle : this.CompareKeyDirection(node.Key!, node.Parent.Key!);
+
+            Transplant(node.Parent!, child, childDirection);
+
+            return null;
+        }
+
+        private void Transplant(Node<TypeKey> node, Node<TypeKey> child, KeyDirection childDirection)
+        {
+            if (node == null)
+            {
+                this.Root = child;
+            }
+            else if (child == null)
+            {
+                RemoveLeaf(node, childDirection);
+            }
+            else if (childDirection == KeyDirection.Left)
+            {
+                node.Left = child;
+            }
+            else
+            {
+                node.Right = child;
+            }
+        }
+
+        private Node<TypeKey>? RemoveColor(Node<TypeKey> node)
+        {
+            int removeCase = GetRemoveCase(node);
+
+            KeyDirection direction = this.CompareKeyDirection(node.Key!, node.Parent!.Key!);
+
+            Node<TypeKey> sibling = direction == KeyDirection.Left ? node.Parent!.Right! : node.Parent!.Left!;
+            Node<TypeKey> closeNephew = direction == KeyDirection.Left ? sibling.Left! : sibling.Right!;
+            Node<TypeKey> farNephew = direction == KeyDirection.Left ? sibling.Right! : sibling.Left!;
+
+            switch (removeCase)
+            {
+                case 1:
+                    sibling.Red = true;
+
+                    return node.Parent;
+
+                case 3:
+                    RemoveCase3(node, closeNephew, direction);
+
+                    break;
+
+                case 4:
+                    RemoveCase4(sibling);
+
+                    break;
+
+                case 5:
+                    RemoveCase5(node, sibling, direction);
+
+                    break;
+
+                case 6:
+                    RemoveCase6(node, farNephew, direction);
+
+                    break;
+            }
+
+            return null;
+        }
+
+        private void RemoveCase3(Node<TypeKey> node, Node<TypeKey>? closeNephew, KeyDirection childDirection)
+        {
+            Node<TypeKey> sibling = childDirection == KeyDirection.Left ? RotateLeft(node.Parent!) : RotateRight(node.Parent!);
+
+            sibling.Red = false;
+            if (childDirection == KeyDirection.Left)
+            {
+                sibling.Left!.Red = true;
+            }
+            else
+            {
+                sibling.Right!.Red = true;
+            }
+
+            sibling = closeNephew!;
+            Node<TypeKey> farNephew = childDirection == KeyDirection.Left ? sibling.Right! : sibling.Left!;
+            if (farNephew != null && farNephew.Red == true)
+            {
+                RemoveCase6(node, farNephew, childDirection);
+                return;
+            }
+
+            closeNephew = childDirection == KeyDirection.Left ? sibling.Left! : sibling.Right!;
+            if (closeNephew != null && closeNephew.Red == true)
+            {
+                RemoveCase5(node, sibling, childDirection);
+                return;
+            }
+
+            RemoveCase4(sibling);
+        }
+
+        private void RemoveCase4(Node<TypeKey> sibling)
+        {
+            sibling.Red = true;
+            sibling.Parent!.Red = false;
+        }
+
+        private void RemoveCase5(Node<TypeKey> node, Node<TypeKey> sibling, KeyDirection childDirection)
+        {
+            sibling = childDirection == KeyDirection.Left ? RotateRight(sibling) : RotateLeft(sibling);
+            Node<TypeKey> farNephew = childDirection == KeyDirection.Left ? sibling.Right! : sibling.Left!;
+
+            sibling.Red = false;
+            farNephew.Red = true;
+
+            RemoveCase6(node, farNephew, childDirection);
+        }
+
+        private void RemoveCase6(Node<TypeKey> node, Node<TypeKey> farNephew, KeyDirection childDirection)
+        {
+            Node<TypeKey> oldParent = node.Parent!;
+            node = childDirection == KeyDirection.Left ? RotateLeft(oldParent) : RotateRight(oldParent);
+            node.Red = oldParent.Red;
+            oldParent.Red = false;
+            farNephew.Red = false;
         }
 
         public string PreOrder()
